f2c13e15ae9166b616ed19180a8b94a9430c4bd3
VishX2 <vishwa.amarajith777@gmail.com>
2026-01-06 11:33:47 +0530

    Enable navigation from search to property details page

--- estate-agent/src/pages/PropertyPage.jsx
+++ estate-agent/src/pages/PropertyPage.jsx
@@ -1,3 +1,16 @@
+import { useParams } from "react-router-dom";
+import data from "../data/properties.json";
+
 export default function PropertyPage() {
-  return <h1>Property Page</h1>;
+  const { id } = useParams();
+  const property = data.properties.find(p => p.id === id);
+
+  if (!property) return <p>Not found</p>;
+
+  return (
+    <div>
+      <h1>{property.location}</h1>
+      <p>{property.description}</p>
+    </div>
+  );
 }
