f05b549086064365ee31a8d385f4b4245347b866
VishX2 <vishwa.amarajith777@gmail.com>
2026-01-06 11:32:48 +0530

    Add property JSON data and display on search page

--- estate-agent/src/pages/SearchPage.jsx
+++ estate-agent/src/pages/SearchPage.jsx
@@ -1,3 +1,16 @@
+import data from "../data/properties.json";
+
 export default function SearchPage() {
-  return <h1>Search Page</h1>;
+  return (
+    <div>
+      <h1>Search Page</h1>
+
+      {data.properties.map(p => (
+        <div key={p.id}>
+          <p>{p.location}</p>
+          <p>£{p.price}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
